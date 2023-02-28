73d2d48add4e48e92451f91cca2009255ef4bc0c
tringapps-rohith <123439228+tringapps-rohith@users.noreply.github.com>
2023-02-28 14:12:38 +0530

    Create calculator.java

---
+++ src/main/java/calculator/calculator/calculator.java
@@ -0,0 +1,22 @@
+package calculator.calculator;
+class Calculator
+{
+        private double a;
+        private double b;
+        public void setA(double a)
+        {
+                this.a=a;
+        }
+        public double getA()
+        {
+                return a;
+        }
+        public void setB(double b)
+        {
+                this.b=b;
+        }
+        public double getB()
+        {
+                return b;
+        }
+}
