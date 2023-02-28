2f3afa641ec15c3a705f7d3538bfec7994184d0b
tringapps-rohith <123439228+tringapps-rohith@users.noreply.github.com>
2023-02-28 14:12:12 +0530

    Update CalcTest.java

--- src/main/java/calculator/calculator/CalcTest.java
+++ src/main/java/calculator/calculator/CalcTest.java
@@ -2,56 +2,6 @@ package calculator.calculator;
 
 import java.util.*;
 import java.util.logging.*;
-class Calculator
-{
-	Logger l=Logger.getLogger("com.api.jar");
-        private double a;
-        private double b;
-        public void setA(double a)
-        {
-                this.a=a;
-        }
-        public double getA()
-        {
-                return a;
-        }
-        public void setB(double b)
-        {
-                this.b=b;
-        }
-        public double getB()
-        {
-                return b;
-        }
-}
-class Addition extends Calculator
-{
-        double calculate()
-        {
-                return getA()+getB();
-        }
-}
-class Subtraction extends Calculator
-{
-        double calculate()
-        {
-                return getA()-getB();
-        }
-}
-class Multiplication extends Calculator
-{
-        double calculate()
-        {
-                return getA()*getB();
-        }
-}
-class Division extends Calculator
-{
-        double calculate()
-        {
-                return getA()/getB();
-        }
-}
 class CalcTest
 {
         public static void main(String ...args)
@@ -63,7 +13,6 @@ class CalcTest
                 double a=sin.nextDouble();
                 l.info("Enter second number\n");
                 double b=sin.nextDouble();
-                Calculator c=new Calculator();
                 int ch;
                 do
                 {
@@ -74,29 +23,29 @@ class CalcTest
                         {
                                 case 1:
                                         Addition add=new Addition();
-                                        c=add;
-                                        c.setA(a);
-                                        c.setB(b);
+                                        
+                                        add.setA(a);
+                                        add.setB(b);
                                         l.log(Level.INFO,()-> "Addition of 2 numbers is... "+add.calculate());
                                         break;
                                 case 2: Subtraction s=new Subtraction();
-                                        c=s;
-                                        c.setA(a);
-                                        c.setB(b);
+                                        
+                                        s.setA(a);
+                                        s.setB(b);
                                         l.log(Level.INFO,()-> "Subtraction of 2 numbers is... "+s.calculate());
                                         break;
                                 case 3:
                                         Multiplication m=new Multiplication();
-                                        c=m;
-                                        c.setA(a);
-                                        c.setB(b);
+                                       
+                                        m.setA(a);
+                                        m.setB(b);
                                         l.log(Level.INFO,()-> "Multiplication of 2 numbers is... "+m.calculate());
                                         break;
                                 case 4:
                                          Division d=new Division();
-                                        c=d;
-                                        c.setA(a);
-                                        c.setB(b);
+                                       
+                                        d.setA(a);
+                                        d.setB(b);
                                         l.log(Level.INFO,()-> "Multiplication of 2 numbers is... "+d.calculate());
                                         break;
 
